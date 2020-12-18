b5b42ed0aa165fc3ac280e7caed303d6641883f4
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-19 10:35:42 +1100

    rm unused items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import Reminders from "./components/Reminders";
+// import Reminders from "./components/Reminders";
 import Travel from "./components/Travel";
 const ReactRouterSetup = () => {
   return (
