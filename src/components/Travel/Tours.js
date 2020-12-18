09f0980e205089a2595a7fee8802b566d38117b3
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-19 10:34:23 +1100

    rm unused items

---
+++ src/components/Travel/Tours.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = () => {
+  return (
+    <>
+      <h1>
+        <Tour />
+      </h1>
+    </>
+  );
+};
+export default Tours;
--- src/components/Travel/index.js
+++ src/components/Travel/index.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from "react";
-import classes from "./Travel.module.css";
+import React, { useState } from "react";
+
 import Loading from "./Loading";
 import Tours from "./Tours";
 
-const url = "https://course-api.com/react-tours-project";
+// const url = "https://course-api.com/react-tours-project";
 
 const DisplayTours = () => {
-  const [tours, setTours] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [tours, setTours] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
   // isLoading ? (<main><Loading/></main>) :
   return (
     <>
       <h1>Please chose the tour you like below:</h1>
       <Loading />
+      <Tours />
     </>
   );
 };
