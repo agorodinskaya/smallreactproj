c503a677f185e59afc901efc1cc8a53dfe5f57e4
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-18 17:45:45 +1100

    rm routes- rework later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import GithubProfiles from "./components/GithubProfiles/GithubProfiles";
-import BookList from "./components/Books/BookList";
+
 import Reminders from "./components/Reminders";
 
 const ReactRouterSetup = () => {
   return (
     <>
-      <Router>
-        {/* <NavBar /> */}
-        <Route path='/'></Route>
-        <Route path='/booklist'>
-          <BookList />
-        </Route>
-        <Route path='/githubprof'>
-          <GithubProfiles />
-        </Route>
-        <Route path='/reminders'>
-          <Reminders />
-        </Route>
-      </Router>
+      <Reminders />
     </>
   );
 };
