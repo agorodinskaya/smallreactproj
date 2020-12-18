e111ee345807d11c429f533b6b06d8996cb936a5
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-18 17:37:28 +1100

    rm unused items e.g. logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-import NavBar from "./components/Navbar/NavBar";
+
 import GithubProfiles from "./components/GithubProfiles/GithubProfiles";
 import BookList from "./components/Books/BookList";
 import Reminders from "./components/Reminders";
+
 const ReactRouterSetup = () => {
   return (
     <>
