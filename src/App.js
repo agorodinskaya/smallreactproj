9e199c8cab9a5916da740ecd007d4c787c8c6cd1
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-22 09:19:39 +1100

    accordion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 
 // import Reminders from "./components/Reminders";
 // import Travel from "./components/Travel";
-import Opinions from "./components/Opinions";
+// import Opinions from "./components/Opinions";
+import DisplayQuestion from "./components/Accordion";
 
 const ReactRouterSetup = () => {
   return (
     <>
       {/* <Reminders /> */}
       {/* <Travel /> */}
-      <Opinions />
+      {/* <Opinions /> */}
+      <DisplayQuestion />
     </>
   );
 };
