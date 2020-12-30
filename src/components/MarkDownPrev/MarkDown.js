3a73f55004bd0715263677e7d7a41e4d3e1ad8a4
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-12-31 02:19:31 +1100

    add color toggle markdown

--- src/components/MarkDownPrev/MarkDown.js
+++ src/components/MarkDownPrev/MarkDown.js
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import classes from "./MarkDown.module.css";
+
+const getLocalStorageTheme = () => {
+  let theme = `${classes.LightTheme}`;
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 const MarkDown = () => {
   const [markdown, setMarkdown] = useState("## markdown preview");
+  const [theme, setTheme] = useState(getLocalStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === `${classes.LightTheme}`) {
+      setTheme(`${classes.DarkTheme}`);
+    } else {
+      setTheme(`${classes.LightTheme}`);
+    }
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <main>
+      <button className={classes.Btn} onClick={toggleTheme}>
+        toggle
+      </button>
       <section className={classes.Markdown}>
         <textarea
           className={classes.Input}
